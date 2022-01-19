707a1606a1fafb510630153f4c0b658fde498531
ViKtOr <viktor.kononenko.2003@mail.ru>
2022-01-19 08:55:19 +0500

    8:54 19.01

---
+++ pract12project/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите а:");
+string a = Console.ReadLine();
+Console.WriteLine("Введите  b:");
+string b = Console.ReadLine();
